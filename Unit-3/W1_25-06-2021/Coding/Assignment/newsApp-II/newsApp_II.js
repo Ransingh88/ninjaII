299a83935863d508b3e51c8fbdb128fc1e662f4a
Ransingh88 <rajaransingh@gmail.com>
2021-06-27 01:31:32 +0530

    newsapp-II

--- Unit-3/W1_25-06-2021/Coding/Assignment/newsApp-II/newsApp_II.js
+++ Unit-3/W1_25-06-2021/Coding/Assignment/newsApp-II/newsApp_II.js
@@ -1,5 +1,6 @@
 var data;
 var topHead = document.getElementById("topHead");
+let catagory
 
 async function main() {
   data = await getNews();
@@ -8,12 +9,13 @@ async function main() {
   showData();
 }
 
-//main();
+main();
 
 async function getNews() {
   let country = document.getElementById("country").value;
-  let catagory = document.getElementById("catagory").value;
-  //console.log('country: ', country);
+  catagory = document.querySelector('input[type="radio"]:checked').value;
+  console.log('catagory: ', catagory);
+  console.log('country: ', country);
   let api = `https://newsapi.org/v2/top-headlines?country=${country}&category=${catagory}&apiKey=bb087c98103a4247a88b7aea6b911876`;
   let promise = await fetch(api);
   data = await promise.json();
@@ -59,7 +61,7 @@ async function getNews() {
 function showData() {
 
   topHead.innerHTML = ''
-  let page_data = data.slice(1, 11);
+  let page_data = data.slice(1, data.length);
   page_data.forEach(function (el) {
     let topHeadline = document.createElement("h1");
     topHeadline.innerText = el.title;
@@ -72,7 +74,26 @@ function showData() {
     let image = document.createElement('img')
     image.src = el.urlToImage
 
-    div.append(topHeadline,image);
+    let desc = document.createElement('p');
+    desc.innerHTML = el.description
+    desc.setAttribute('class','description')
+
+    let publishedAt = document.createElement('p');
+    publishedAt.setAttribute('class','publishedAt')
+    publishedAt.innerHTML = el.publishedAt
+
+    let content = document.createElement('p');
+    content.setAttribute('class','content')
+    content.innerHTML = el.content
+
+    let source = document.createElement('p');
+    source.setAttribute('class','source')
+    source.innerHTML = `source:${el.source.name}`
+
+    let heading = document.getElementById('heading');
+    heading.innerText = `${catagory} Articles` 
+
+    div.append(topHeadline,source,publishedAt,desc,image,content);
 
 
     topHead.append(div);
