1234e076308e1a11c632b319f5a00e55ca47961b
Ransingh88 <rajaransingh@gmail.com>
2021-07-07 23:44:52 +0530

    mobile os

---
+++ Unit-3/W2_06-07-2021/Assignment/react-mobile-os/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <div>
+      <h1>MobileOperation System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Wimdows Phone</li>
+      </ul>
+      
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+    </div>
+  );
+}
+
+export default App;
